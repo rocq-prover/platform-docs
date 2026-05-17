fcf19630fa97fac31bb34169fe430aac7b8c5c80
Thomas Lamiaux <85848641+thomas-lamiaux@users.noreply.github.com>
2026-05-17 16:38:36 +0200

    Update src/metaprogramming/ltac2/tutorial_ltac2_for_ltac1_users.v

--- src/metaprogramming/ltac2/tutorial_ltac2_for_ltac1_users.v
+++ src/metaprogramming/ltac2/tutorial_ltac2_for_ltac1_users.v
@@ -256,7 +256,9 @@ Proof.
 Qed.
 
 (** In Ltac2, this is now written with the syntax [tac1 > [tac21 | tac22]] in order
-    to avoid syntax conflict with ???. Moreover, [tac1; tac2; [tac31 | tac32]]
+    to avoid confusion between chaining tactics and dispatching. 
+    The latter is asymetric and does not compose opposite to [;]. 
+    Moreover, [tac1; tac2; [tac31 | tac32]]
     is now parsed as [tac1; (tac2; [tac31 | tac32])] as Ltac2 no longer
     automatically delays tactic execution.
 
